2ecac194d34bf3ab11c0e3bb399e39a3b7329bca
bee <Bee0_0@icloud.com>
2020-03-27 18:01:16 +0800

    support disconnect

--- handler/wshandler.go
+++ handler/wshandler.go
@@ -24,27 +24,33 @@ var upgrader = websocket.Upgrader{
 // WSHandler : websocket接口
 func WSHandler(w http.ResponseWriter, r *http.Request) {
 	conn, _ := upgrader.Upgrade(w, r, nil)
+	for {
+		_, rawData, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
 
-	_, rawData, err := conn.ReadMessage()
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+		data := string(rawData)
+		resp := WebSocketResp{}
 
-	data := string(rawData)
-	resp := WebSocketResp{}
+		if err := json.Unmarshal([]byte(data), &resp); err != nil {
+			return
+		}
 
-	if err := json.Unmarshal([]byte(data), &resp); err != nil {
-		return
-	}
+		if resp.Operation == 0 {
+			// Operation == 0 means user is connecting
+			util.ConnMap[util.MD5(resp.Username)] = conn
+			fmt.Println("user: " + resp.Username + " connected!")
+		} else if resp.Operation == 1 {
+			// Operation == 1 means user is logging out
+			delete(util.ConnMap, util.MD5(resp.Username))
+			fmt.Println("user: " + resp.Username + " disconnected!")
+		} else {
+			panic("Unknown Operation!")
+		}
 
-	if resp.Operation == 0 {
-		util.ConnMap[util.MD5(resp.Username)] = conn
-		fmt.Println("user: " + resp.Username + " connected!")
-	} else if resp.Operation == 1 {
-		delete(util.ConnMap, util.MD5(resp.Username))
-	} else {
-		panic("Unknown Operation!")
+		fmt.Println(util.ConnMap)
 	}
 
 }
